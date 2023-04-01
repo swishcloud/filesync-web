6549efd923dd852f320444b5d237ecbf171897a4
Your Name <you@example.com>
2023-04-01 12:12:41 +0800

    update docker/build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ if [ -d ".dist" ];then
     rm .dist -r
 fi
 mkdir .dist
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./.dist/app
+https_proxy=http://192.168.29.9:8081 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./.dist/app
 cp templates  ./.dist -r
 cp static  ./.dist -r
 cp migrations  ./.dist -r
